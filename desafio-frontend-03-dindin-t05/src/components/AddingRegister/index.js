c2e1c0acd6231e3c97a790a2f80f4cf249b7a50d
Leonardo Queiroz <leeonardo.queiroz@outlook.com>
2022-04-12 15:34:58 -0300

    Teste

--- desafio-frontend-03-dindin-t05/src/components/AddingRegister/index.js
+++ desafio-frontend-03-dindin-t05/src/components/AddingRegister/index.js
@@ -1,41 +1,45 @@
 import './style.css';
 import btnClose from '../../assets/close.svg';
 
-function AddingRegister() {
+function AddingRegister({ open, }) {
 
     return (
-        <div className='modal-container'>
-            <div className='modal'>
-                <div className='title'>
-                    <h1>Adicionar registro</h1>
-                    <img src={btnClose} alt='close-button' />
-                </div>
+        <>
+            {open &&
+                < div className='modal-container'>
+                    <div className='modal'>
+                        <div className='title'>
+                            <h1>Adicionar registro</h1>
+                            <img src={btnClose} alt='close-button' />
+                        </div>
 
-                <div className='btns'>
-                    <button className='btn-in'>Entrada</button>
-                    <button className='btn-out'>Saída</button>
+                        <div className='btns'>
+                            <button className='btn-in'>Entrada</button>
+                            <button className='btn-out'>Saída</button>
+                        </div>
+                        <form>
+                            <div className='input-value'>
+                                <label>Valor</label>
+                                <input type='number'></input>
+                            </div>
+                            <div className='input-category'>
+                                <label>Categoria</label>
+                                <select></select>
+                            </div>
+                            <div className='input-date'>
+                                <label>Data</label>
+                                <input type='date'></input>
+                            </div>
+                            <div className='input-description'>
+                                <label>Descrição</label>
+                                <input type='text'></input>
+                            </div>
+                            <button type='submit'>Confirmar</button>
+                        </form>
+                    </div>
                 </div>
-                <form>
-                    <div className='input-value'>
-                        <label>Valor</label>
-                        <input type='number'></input>
-                    </div>
-                    <div className='input-category'>
-                        <label>Categoria</label>
-                        <select></select>
-                    </div>
-                    <div className='input-date'>
-                        <label>Data</label>
-                        <input type='date'></input>
-                    </div>
-                    <div className='input-description'>
-                        <label>Descrição</label>
-                        <input type='text'></input>
-                    </div>
-                    <button type='submit'>Confirmar</button>
-                </form>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
